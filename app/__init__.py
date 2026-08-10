085974cb141ecee36c16378ff18c6e8b651da4b6
xhinoo97-svg <marxhino97@gmail.com>
2026-08-10 06:07:47 +0200

    Install authoritative black-pixel support policy

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ os.environ.setdefault("OPENCV_FORCE_DNN_ENGINE", "1")
 # authoritative for verified damaged seed pixels.
 from app.same_canvas_seed_support_policy import install_same_canvas_seed_support_policy
 from app.same_canvas_seed_precision_policy import install_same_canvas_seed_precision_policy
+from app.same_canvas_black_support_policy import install_same_canvas_black_support_policy
 from app.multi_reference_runtime_policy import install_multi_reference_runtime_policy
 from app.automatic_quality_policy import install_automatic_quality_policy
 from app.observed_target_photometric_policy import install_observed_target_photometric_policy
@@ -25,6 +26,7 @@ from app.preflight_selective_deblur_policy import install_preflight_selective_de
 
 install_same_canvas_seed_support_policy()
 install_same_canvas_seed_precision_policy()
+install_same_canvas_black_support_policy()
 install_multi_reference_runtime_policy()
 install_automatic_quality_policy()
 install_observed_target_photometric_policy()
