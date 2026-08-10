ee0347603d7b83daa96374cd9737a90dca4a5a3f
xhinoo97-svg <marxhino97@gmail.com>
2026-08-10 06:10:33 +0200

    Remove redundant black support wrapper

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,6 @@ os.environ.setdefault("OPENCV_FORCE_DNN_ENGINE", "1")
 # authoritative for verified damaged seed pixels.
 from app.same_canvas_seed_support_policy import install_same_canvas_seed_support_policy
 from app.same_canvas_seed_precision_policy import install_same_canvas_seed_precision_policy
-from app.same_canvas_black_support_policy import install_same_canvas_black_support_policy
 from app.multi_reference_runtime_policy import install_multi_reference_runtime_policy
 from app.automatic_quality_policy import install_automatic_quality_policy
 from app.observed_target_photometric_policy import install_observed_target_photometric_policy
@@ -27,7 +26,6 @@ from app.reference_guided_seed_policy import install_reference_guided_seed_polic
 
 install_same_canvas_seed_support_policy()
 install_same_canvas_seed_precision_policy()
-install_same_canvas_black_support_policy()
 install_multi_reference_runtime_policy()
 install_automatic_quality_policy()
 install_observed_target_photometric_policy()
